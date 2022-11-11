a10d4fd3304f9fcfab32445c6dee60ec3a0f9451
Nir Meron <96244260+nickmeron@users.noreply.github.com>
2022-11-11 13:29:08 +0200

    Update script.js

--- Assignment - JS – Functions/script.js
+++ Assignment - JS – Functions/script.js
@@ -1,19 +1,20 @@
-let num1 = 0
-let num2 = 0
+let num1,num2 = 0
+
 
 function exec(){
   num1 = prompt("Enter First Number");
   num2 = prompt("Enter Second Number");
   Number(num1);
   Number(num2);
-  calculate();
+  calculate(num1,num2);
 }
 
-function calculate(){
-  if (num1 % num2 != null && num1 % num2 > 0){
-      alert("Residue exist!")
-  }
-  else{
-    alert("No Residue!")
-  }
+function calculate(number1,number2){
+    let response = number1 % number2;
+    if (response != 0){
+      alert("Residue is " + response)
+    }
+    else{
+        alert("No Residue!")
+    }
 }
